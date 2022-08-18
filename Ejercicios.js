10b5608a0fd79a3fc36672d41a23bd41288559c4
joaquinquiroga22 <joaquinquiroga22@gmail>
2022-08-18 14:07:20 -0300

    first commit

---
+++ Ejercicios.js
@@ -0,0 +1,25 @@
+function invocarCallback(cb) {
+    // Invoca al callback `cb`
+    // Aqui tu codigo
+}
+
+function sumarArray(numeros, cb) {
+    // Suma todos los números enteros (int/integers) de un array ("numeros")
+    // Pasa el resultado a `cb`
+    // No es necesario devolver nada
+    // Aqui tu codigo
+}  
+
+function forEach(array, cb) {
+    // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
+    // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
+    // Aqui tu codigo
+}  
+
+function map(array, cb) {
+    // Crea un nuevo array
+    // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
+    // El nuevo array debe tener la misma longitud que el array del argumento
+    // Aqui tu codigo
+}  
+
